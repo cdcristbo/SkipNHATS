24a0d80ceb02f419b27bc80767ce5201a1f7102b
ccristbo <ccristbo@gmail.com>
2024-02-25 22:55:19 -0500

    changes new function to skip files by pattern

--- Scripts/SkipbyPattern.R
+++ Scripts/SkipbyPattern.R
@@ -6,7 +6,12 @@ library(stringr)
 # Specify the path to your Excel file
 
 # Set common base path
-base_path <- "C:/Users/ccris/Dropbox (University of Michigan)/carlos/Work/Nhats/workNhats/"
+base_path <- "C:/Users/ccris/Dropbox (University of Michigan)/carlos/Work/Nhats/SkipNHATS/"
+
+# Read the necessary Excel files
+Part2 <- read_excel(paste0(base_path, "SkipNHATS/datasets/SkipDataset/NHATSNationalStudyRound1SpecWriterExchange.xlsx"), sheet = "ItemResponse")
+fullList <- read_excel(paste0(base_path, "SkipNHATS/datasets/SkipDataset/NHATSNationalStudyRound1SpecWriterExchange.xlsx"), sheet = "Item")
+trueNames <- read_excel(paste0(base_path, "SkipNHATS/datasets/SkipDataset/NHATS_R1_Crosswalk_between_Instruments_and_Codebook_0.xlsx"))
 
 load(paste0(base_path, "outcomes/FinalPresent.RData"))
 getRowsGroup1 <- paste0(base_path, "Functions/getRowsGroup1.R")
@@ -25,7 +30,10 @@ file_list = file_list[1]
 # Create an empty dataframe to store the results
 result_df <- data.frame(variable = character(0), round1Inaplicable = character(0))
 data <- read_dta(file_list)
-patternData = FinalPresent
+
+#FinalPresent<- processData(Part2, fullList, trueNames)
+#patternData = FinalPresent
+patternData = processData(Part2, fullList, trueNames)
 
 patternData = patternData %>% 
   mutate(pattern = ifelse(Variable.name==name1,NA,pattern)) %>% 
@@ -64,4 +72,5 @@ for (i in seq_len(nrow(patternData))) {
 
 results_df = results_df %>% 
   distinct()
+  View(results_df)
 #save(results_df, file = paste0(base_path, "outcomes/results_df.RData"))
